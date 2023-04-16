881dd76316b6ba349be70443955b7e18e1b28750
Dmytro Petrenko <petrenko_dmytro@ukr.net>
2023-04-16 22:03:28 +0300

    create components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,48 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { GlobalStyle } from './GlobalStyle';
+import { Layout } from './Layout/Layout';
+
+export class App extends Component {
+  // Стан застосунку
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+  // метод класу збору статистики
+  onLeaveFeedback = e => {
+    console.log(e);
+  };
+  //метод класу загальної кількості зібраних відгуків з усіх категорій
+  countTotalFeedback = ({ good, neutral, bad }) => good + neutral + bad;
+  //метод классу відсоток позитивних відгуків
+  countPositiveFeedbackPercentage = ({ good, neutral, bad }) => {
+    const TotalFeedback = this.countTotalFeedback(this.state);
+    return Math.round((good / TotalFeedback) * 100);
+  };
+
+  render() {
+    return (
+      <Layout>
+        <h2>Please leave feedback</h2>
+        <div>
+          <button name='good' onClick={this.onLeaveFeedback}>good</button>
+          <button>neutral</button>
+          <button>bad</button>
+        </div>
+
+        <h2>Statistics</h2>
+        <div>
+          <p>good</p>
+          <p>neutral</p>
+          <p>bad</p>
+          <p>Total</p>
+          <p>Positive feedback</p>
+          <p>There is no feedback</p>
+        </div>
+
+        <GlobalStyle />
+      </Layout>
+    );
+  }
+}
